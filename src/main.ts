1be9580e9485088d0a08172c2cc365c10c41a5b8
En0ri4n <50873744+En0ri4n@users.noreply.github.com>
2026-05-20 09:50:09 +0200

    Switched from express to fastify

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { NestFastifyApplication } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule);
   const configService = app.get(ConfigService);
   app.enableCors();
 
