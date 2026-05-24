1f55a25bade11ca5a60d655d68e3338239dbb3c9
Enorian <enorian.rajo@gmail.com>
2026-05-24 18:24:51 +0200

    Move helpers classes

--- src/helpers/message-interceptor.ts
+++ src/helpers/message-interceptor.ts
@@ -1,14 +1,9 @@
-import {
-  Injectable,
-  NestInterceptor,
-  ExecutionContext,
-  CallHandler,
-} from '@nestjs/common';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import {CallHandler, ExecutionContext, Injectable, NestInterceptor,} from '@nestjs/common';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Injectable()
-export class TransformInterceptor implements NestInterceptor {
+export class MessageTransformerInterceptor implements NestInterceptor {
   intercept(_context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       map((data) => {
